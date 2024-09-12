f26b2eba0f512bc40e7e637f2c4ba9428046751d
P3N5IVE_CH3PE <P3N5IVE_CH3PE@protonmail.com>
2024-09-11 21:23:33 -0700

    nothing new

--- rust_ownership/src/main.rs
+++ rust_ownership/src/main.rs
@@ -1,3 +1,38 @@
+
+// Safety is the absence of undefined behavior.
+
+//fn read(y: bool){ 
+//        if y { 
+//                println!("y is true!");
+//                }
+
+//        }
+
+
+// Unsafe version of the former due to moving the call to read before the definition of x
+//fn read(y: bool){
+//	if y {
+//	 println!("y is true!");
+//	}
+//}
 fn main() {
-    println!("Hello, world!");
+//	let x = true;
+//	read(x); 
+//	read(x);
+//	let x = true;
+
+ 
+	let a = Box::new([0; 1_000_000]);
+	let b = a;
+
+	for C in b () {
+
+		println!("{C}");
+	}
+
+
 }
+
+
+
+
